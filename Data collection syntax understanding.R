5679c63c4cea0ee103a9de90350c947bbbf9b7e0
Asad Ullah <asad2011ai@gmail.com>
2022-02-09 14:44:32 +0500

    solve some questions

--- Data collection syntax understanding.R
+++ Data collection syntax understanding.R
@@ -1,3 +1,4 @@
+###############################
 # check working directory
 getwd()
 
@@ -16,6 +17,8 @@ getwd()
 setwd('../')
 getwd()
 
+
+###############################
 # if you download file import parameter of 
 # this function is url, destfile, method
 
@@ -30,3 +33,53 @@ cameradata <- read.csv("./data/annural_enterprise_survey.csv")
 str(cameradata)
 datedownload <- date()
 datedownload
+
+###############################
+# read file
+cameradata <- read.table("./data/cameras.csv", sep = ',', header = TRUE)
+head(cameradata)
+
+#read.csv
+cameradata <- read.csv("./data/cameras.csv")
+head(cameradata)
+library(data.table)
+?fread
+################################################################
+# excel file
+install.packages("xlsx")
+library(xlsx)
+cameradata <- read.xlsx("./data/cameras.xlsx", sheetIndex=1, header=TRUE)
+head(cameradata)
+colIndex <- 2:3
+rowIndex <- 1:4
+cameradatasubset <- read.xlsx("./data/cameras.xlsx", sheetIndex=1, colIndex = colIndex, rowIndex=rowindex)
+cameradatasubset
+#---------------------
+install.packages("readxl")
+library("readxl")
+readxl_example()
+xlsxexample <- readxl_example("datasets.xlsx")
+diris <- read_excel(xlsxexample)
+dd <- read_excel("cameras.xlsx")
+#-------------------
+# write in excel file
+devtools::install_github("repensci/writexl")
+library(writexl)
+write_xlsx(df, "file_formated.xlsx")
+#------------------
+?read_excel()
+# install.packages("openxlsx")
+#########################################
+# web scraping
+#--------------
+# reading webpage
+install.packages(revest)
+library(rvest)
+#----------------
+theurl <- "https://en.wikipedia.org/wiki/Brazil_national_football_team"
+file <- read_html(theurl)
+
+# read table from webpage
+tables <- html_nodes(file, "table")
+table <- html_table(tables[3], fill = TRUE)
+print(table)
